3a17ae38fd50743a4ea26489bbd00d56f22c48ca
Oleksii Pronyakin <Oleksii_Pronyakin@epam.com>
2014-09-29 14:33:48 +0300

    add todo functionality

--- app/js/helper.js
+++ app/js/helper.js
@@ -14,6 +14,20 @@ define([], function(){
             ChildClass.prototype = new Tmp();
             ChildClass.prototype.constructor = ChildClass;
             ChildClass.super = ParentClass.prototype;
+        },
+
+        isEmpty: function (obj) {
+
+            if (!obj) return true;
+
+            if (obj.length > 0)    return false;
+            if (obj.length === 0)  return true;
+
+            for (var key in obj) {
+                if (hasOwnProperty.call(obj, key)) return false;
+            }
+
+            return true;
         }
     };
 });
--- app/js/todo/todo.js
+++ app/js/todo/todo.js
@@ -1,36 +1,117 @@
 'use strict';
 
-define(['app'], function(app){
+define(['app', 'helper'], function(app, helper){
     app.controller('TODO', function ($scope) {
+        angular.extend($scope, {
+            'todoText': '',
+            'allChecked': false,
+            'filterByChecked': '',
+            'filterByCompleted': '',
+            'filterByText': ''
+        });
+
+        $scope.todoFilter = function () {
+            var obj = {};
+
+            if ($scope.filterByChecked !== '') {
+                obj.checked = $scope.filterByChecked;
+            }
+
+            if ($scope.filterByCompleted !== '') {
+                obj.completed = $scope.filterByCompleted;
+            }
+
+            return helper.isEmpty(obj) ? null : obj;
+        };
         $scope.todos = [
             {
                 'text': 'some text 1',
+                'checked': false,
                 'completed': false
             },
             {
                 'text': 'some text 2',
+                'checked': false,
                 'completed': false
             },
             {
                 'text': 'some text 3',
+                'checked': false,
                 'completed': true
             },
             {
                 'text': 'some text 4',
+                'checked': false,
                 'completed': false
             },
             {
                 'text': 'some text 5',
+                'checked': false,
                 'completed': false
             },
             {
                 'text': 'some text 6',
+                'checked': false,
                 'completed': false
             }
         ];
 
+        function checkExistTodo(todoList, todoText) {
+            for (var i=0; i < todoList.length; i++) {
+                if (todoList[i].text === todoText) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
         $scope.removeTodo = function (index) {
             $scope.todos.splice(index, 1);
+        };
+
+        $scope.addTodo = function (text) {
+            if (!text) return false;
+            var exist;
+            $scope.todos.push({
+                'completed': false,
+                'text': text
+            });
+        };
+
+        $scope.addTodoDisabled = function () {
+            if ($scope.todoText.length === 0)
+                return true;
+
+            if (checkExistTodo($scope.todos, $scope.todoText))
+                return true;
+
+            return false;
+        };
+
+        $scope.enableChangeTodo = function () {
+            console.log(this);
+        };
+
+        $scope.toggleTextEdit = function (event, editable) {
+            var $element = angular.element(event.srcElement);
+
+            if (editable) {
+                $element.removeAttr('readonly');
+            } else {
+                $element.attr('readonly', true);
+            }
+        };
+
+        $scope.removeByProp = function (prop, removeVal) {
+            $scope.todos = $scope.todos.filter(function (todo) {
+                return todo[prop] != removeVal;
+            });
+        };
+
+        $scope.markAll = function (checked) {
+            $scope.todos.forEach(function (todo) {
+                todo.checked = checked;
+            });
         };
     });
 });
